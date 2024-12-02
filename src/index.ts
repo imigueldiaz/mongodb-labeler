ff6c94a45a7db30d1d446fbe56c4037debdfd5f7
Ignacio de Miguel Diíaz <imigueldiaz@gmail.com>
2024-12-02 10:54:10 +0100

    Add export for errors/index.js module

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export * from "./util/util.js";
 export * from "./util/errorUtils.js";
 export type { LabelerOptions } from "./LabelerServer.js";
 export * from "./util/types.js";
+export * from "./errors/index.js";
